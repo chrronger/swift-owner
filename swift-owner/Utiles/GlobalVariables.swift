090dd8eee6dd58132dafad5dec1a4ec2654d9456
sen <sen@sendeiMac.local>
2016-09-19 17:22:59 +0800

    add

--- swift-owner/Utiles/GlobalVariables.swift
+++ swift-owner/Utiles/GlobalVariables.swift
@@ -19,3 +19,67 @@ struct globalVariables {
     static let height: CGFloat =  64
 }
 
+
+
+
+/** 用于搜索 （text输入文本 ）  需替换URL  */
+func requestSuggestionsURL(text: String) -> URL {
+    let netText = text.addingPercentEncoding(withAllowedCharacters: CharacterSet())!
+    let url = URL.init(string: "https://api.bing.com/osjson.aspx?query=\(netText)")!
+    return url
+}
+/** 网络连接 */
+func showNotification() {
+    print("Please check your internet connection")
+}
+
+
+/** globalvaribales enum */
+enum stateOfVC {
+    case minimized
+    case fullScreen
+    case hidden
+}
+
+/**
+ 手机型号枚举
+ */
+enum iPhoneModel {
+    
+    case iPhone4
+    case iPhone5
+    case iPhone6
+    case iPhone6p
+    case iPad
+    
+    /**
+     获取当前手机型号
+     
+     - returns: 返回手机型号枚举
+     */
+    static func getCurrentModel() -> iPhoneModel {
+        switch SCREEN_HEIGHT {
+        case 480:
+            return .iPhone4
+        case 568:
+            return .iPhone5
+        case 667:
+            return .iPhone6
+        case 736:
+            return .iPhone6p
+        default:
+            return .iPad
+        }
+    }
+}
+
+/**
+ 获取缓存的json数据
+ 
+ */
+
+
+
+
+
+
